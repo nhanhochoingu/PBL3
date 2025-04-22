cf1c6cc8197e4b0b3ec14e2a643a775dc392c0df
nhannhan123 <truonghoainhan2005@gmail.com>
2025-04-22 08:30:37 +0700

    Add project files.

---
+++ PBL3/VIEW/Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL3
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void butExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Login_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn muốn thoát ứng dụng ?", "Thông báo thoát", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void butLogin_Click(object sender, EventArgs e)
+        {
+            Management manage = new Management();
+            this.Hide();
+            manage.ShowDialog();
+            this.Show();
+        }
+    }
+}
